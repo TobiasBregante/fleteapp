b1ee711b8accd6b783e8be888738f3637529b125
tobiasbregante <tobiasbregante2018@gmail.com>
2021-06-27 02:39:54 -0300

    scripts repair x2

--- components/layout/_index.jsx
+++ components/layout/_index.jsx
@@ -11,6 +11,20 @@ const Layout = ({ titlePage, children }) => {
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>{ titlePage }</title>
+            <meta name="description" content="Llegó la app de delivery y fletes más popular a la argentina. Hacé tu delivery en un instante" />
+            <meta name="title" content="Hacé tu pedido | FleteApp"/>
+
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://fleteapp.herokuapp.com/"/>
+            <meta property="og:title" content="Hacé tu pedido | FleteApp"/>
+            <meta property="og:description" content="Llegó la app de delivery y fletes más popular a la argentina. Hacé tu delivery en un instante"/>
+            <meta property="og:image" content="img/banner-box.jpg"/>
+
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://fleteapp.herokuapp.com/"/>
+            <meta property="twitter:title" content="Hacé tu pedido | FleteApp"/>
+            <meta property="twitter:description" content="Llegó la app de delivery y fletes más popular a la argentina. Hacé tu delivery en un instante"/>
+            <meta property="twitter:image" content="img/banner-box.jpg"/>
             <link 
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
                 rel="stylesheet" 
