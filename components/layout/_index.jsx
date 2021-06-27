2341a6e39d55ba4c24f079fe40458c7e1e0beed0
tobiasbregante <tobiasbregante2018@gmail.com>
2021-06-27 02:11:59 -0300

    scripts repair

--- components/layout/_index.jsx
+++ components/layout/_index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Footer from './_footer'
 import Header from './_header'
+import Script from 'next/script'
 
 const Layout = ({ titlePage, children }) => {
     return(
@@ -16,12 +17,12 @@ const Layout = ({ titlePage, children }) => {
                 integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
                 crossOrigin="anonymous"
             />
-            <script 
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
-                crossOrigin="anonymous"
-            />
         </Head>
+        <Script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+            crossOrigin="anonymous"
+        />
         <main className="container-fluid">
             <Header/>
             { children }
