96284bf13a0f6ab8ea26c45cd7de8317dd8d2b5c
tobiasbregante <tobiasbregante2018@gmail.com>
2021-08-15 23:21:16 -0300

    FA create panel admin and auth user connection to server and create endpoints to consumer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    SERVER_URL: 'http://localhost:4000'
+    SERVER_URL: 'https://fleteapp-api.herokuapp.com',
+    SERVER_URL_dev: 'http://localhost:4000',
+    SERVER_URL_prod: 'https://fleteapp-api.herokuapp.com'
   }
 }
