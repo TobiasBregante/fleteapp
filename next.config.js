bc3170b7c36c7cc3ab4f86fa2f206de603a4317f
Tobías Nazareno Bregante <50115525+TobiasBregante@users.noreply.github.com>
2023-10-25 14:54:36 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    SERVER_URL: 'https://fleteapp-api.herokuapp.com',
-    SERVER_URL_dev: 'http://localhost:4000',
-    SERVER_URL_prod: 'https://fleteapp-api.herokuapp.com',
-    CLIENT_SERVER_URL: 'http://localhost:3000',
-    CLIENT_SERVER_URL_PROD: 'https://fleteapp.herokuapp.com',
-    CLIENT_SERVER_URL_DEV: 'http://localhost:3000'
-  }
 }
